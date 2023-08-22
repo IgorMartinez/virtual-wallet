ea0e61a5cd1f88dad465879ffcc88769df32c6d5
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-08-22 18:40:41 -0300

    Create auth controller to manage auth routes

---
+++ api/src/main/java/br/com/igormartinez/virtualwallet/controllers/AuthController.java
@@ -0,0 +1,38 @@
+package br.com.igormartinez.virtualwallet.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.igormartinez.virtualwallet.data.dto.RegistrationDTO;
+import br.com.igormartinez.virtualwallet.data.dto.UserDTO;
+import br.com.igormartinez.virtualwallet.data.security.AccountCredentials;
+import br.com.igormartinez.virtualwallet.data.security.Token;
+import br.com.igormartinez.virtualwallet.services.AuthService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    AuthService service;
+
+    @PostMapping("/signup")
+    public UserDTO signup(@RequestBody RegistrationDTO registrationDTO) {
+        return service.signup(registrationDTO);
+    }
+
+    @PostMapping("/signin")
+    public Token signin(@RequestBody AccountCredentials accountCredentials) {
+        return service.signin(accountCredentials);
+    }
+    
+    @PutMapping("/refresh")
+    public Token refresh(@RequestHeader("Authorization") String refreshToken) {
+        return service.refresh(refreshToken);
+    }
+}
