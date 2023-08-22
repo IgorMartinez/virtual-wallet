be3183991c5fae3fb37efbc593f85b486c86e486
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-08-22 18:34:59 -0300

    Change migration to add columns to validate a user

--- api/src/main/resources/db/migration/V2__Create_table_users.sql
+++ api/src/main/resources/db/migration/V2__Create_table_users.sql
@@ -5,6 +5,10 @@ CREATE TABLE users (
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
     role INTEGER,
+    account_non_expired BOOLEAN NOT NULL,
+    account_non_locked BOOLEAN NOT NULL,
+    credentials_non_expired BOOLEAN NOT NULL,
+    enabled BOOLEAN NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (role) REFERENCES roles(id) ON DELETE CASCADE
 );
