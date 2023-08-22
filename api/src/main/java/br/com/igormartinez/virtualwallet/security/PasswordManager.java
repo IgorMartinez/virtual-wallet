d0eef2373fa14a327ed7eecc3916e818e4878607
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-08-22 18:38:36 -0300

    Add class to manage the password logic

---
+++ api/src/main/java/br/com/igormartinez/virtualwallet/security/PasswordManager.java
@@ -0,0 +1,33 @@
+package br.com.igormartinez.virtualwallet.security;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordManager {
+
+    public PasswordManager() {}
+
+    public PasswordEncoder getDefaultPasswordEncoder(){
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+		Pbkdf2PasswordEncoder pbkdf2PasswordEncoder = new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+		encoders.put("pbkdf2", pbkdf2PasswordEncoder);
+		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+		passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2PasswordEncoder);
+
+        return passwordEncoder;
+    }
+
+    public String encodePassword(String rawPassword) {
+        PasswordEncoder passwordEncoder = getDefaultPasswordEncoder();
+        String encodedString = passwordEncoder.encode(rawPassword);
+        return StringUtils.substringAfter(encodedString, "}"); // {pbkdf2}f88...
+    }
+}
