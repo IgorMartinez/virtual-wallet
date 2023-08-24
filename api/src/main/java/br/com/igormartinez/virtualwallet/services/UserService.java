97a9da4dce93837b576c534b38f9ba8ac5849b6d
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-08-24 16:19:51 -0300

    Remove unnecessary code

--- api/src/main/java/br/com/igormartinez/virtualwallet/services/UserService.java
+++ api/src/main/java/br/com/igormartinez/virtualwallet/services/UserService.java
@@ -5,23 +5,21 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import br.com.igormartinez.virtualwallet.repositories.RoleRepository;
 import br.com.igormartinez.virtualwallet.repositories.UserRepository;
-import br.com.igormartinez.virtualwallet.security.PasswordManager;
 
 @Service
 public class UserService implements UserDetailsService {
 
     private final UserRepository repository;
 
-    public UserService(UserRepository repository, PasswordManager passwordManager, RoleRepository roleRepository) {
+    public UserService(UserRepository repository) {
         this.repository = repository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return repository.findByEmail(email)
-            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            .orElseThrow(() -> new UsernameNotFoundException("User not found."));
     }
     
 }
