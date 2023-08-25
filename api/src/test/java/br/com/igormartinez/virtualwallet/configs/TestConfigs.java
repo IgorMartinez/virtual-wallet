23b8fb1d022104dc39b37251e3e3206d4c56b63d
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-08-25 12:32:25 -0300

    Add TestConfigs class

---
+++ api/src/test/java/br/com/igormartinez/virtualwallet/configs/TestConfigs.java
@@ -0,0 +1,13 @@
+package br.com.igormartinez.virtualwallet.configs;
+
+public class TestConfigs {
+    public static final int SERVER_PORT = 8888;
+
+    public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    public static final String HEADER_PARAM_ORIGIN = "Origin";
+
+    public static final String CONTENT_TYPE_JSON = "application/json";
+
+    public static final String EXPIRED_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiQ09NTU9OIiwiaWF0IjoxNjkyOTc2MDM4LCJleHAiOjE2OTI5Nzk2MzgsInN1YiI6ImF1dGhjb250cm9sbGVyQGludGVncmF0aW9uLnRlc3QiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAifQ.y9Md8uI1CFTEf_mGQ7kLWJphHNt8SYekQi6dnvKW4bI";
+    public static final String EXPIRED_REFRESH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiQ09NTU9OIiwiaWF0IjoxNjkyOTc2MDM4LCJleHAiOjE2OTI5ODY4MzgsInN1YiI6ImF1dGhjb250cm9sbGVyQGludGVncmF0aW9uLnRlc3QifQ.EqDMmX6sJC-bpCSjumnRncO2NvemDHE59q0fsIotIa4";
+}
