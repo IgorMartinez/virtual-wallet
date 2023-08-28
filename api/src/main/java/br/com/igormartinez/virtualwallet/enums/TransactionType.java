80e637f241374ecfdbacb6cc3dfb19830c8a2d0f
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-08-28 17:44:58 -0300

    Create Entity and enum to control the types of a transaction

---
+++ api/src/main/java/br/com/igormartinez/virtualwallet/enums/TransactionType.java
@@ -0,0 +1,7 @@
+package br.com.igormartinez.virtualwallet.enums;
+
+public enum TransactionType {
+    DEPOSIT,
+    WITHDRAWAL,
+    TRANSFER
+}
---
+++ api/src/main/java/br/com/igormartinez/virtualwallet/models/Transaction.java
@@ -0,0 +1,147 @@
+package br.com.igormartinez.virtualwallet.models;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+import br.com.igormartinez.virtualwallet.enums.TransactionType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "counterparty_user_id")
+    private User counterpartyUser;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private TransactionType type;
+
+    @Column(name = "value", nullable = false)
+    private BigDecimal value;
+
+    @Column(name = "datetime", nullable = false)
+    private ZonedDateTime datetime;
+
+    public Transaction() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getCounterpartyUser() {
+        return counterpartyUser;
+    }
+
+    public void setCounterpartyUser(User counterpartyUser) {
+        this.counterpartyUser = counterpartyUser;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public ZonedDateTime getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(ZonedDateTime date) {
+        this.datetime = date;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((user == null) ? 0 : user.hashCode());
+        result = prime * result + ((counterpartyUser == null) ? 0 : counterpartyUser.hashCode());
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        result = prime * result + ((datetime == null) ? 0 : datetime.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Transaction other = (Transaction) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (user == null) {
+            if (other.user != null)
+                return false;
+        } else if (!user.equals(other.user))
+            return false;
+        if (counterpartyUser == null) {
+            if (other.counterpartyUser != null)
+                return false;
+        } else if (!counterpartyUser.equals(other.counterpartyUser))
+            return false;
+        if (type != other.type)
+            return false;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        if (datetime == null) {
+            if (other.datetime != null)
+                return false;
+        } else if (!datetime.equals(other.datetime))
+            return false;
+        return true;
+    }
+}
