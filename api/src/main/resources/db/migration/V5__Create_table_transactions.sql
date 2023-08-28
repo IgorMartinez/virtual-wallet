d228054bcc62229ff7f9e690925c9d4452241e0b
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-08-28 17:53:42 -0300

    Change table transaction script to add timezone in datetime

--- api/src/main/resources/db/migration/V5__Create_table_transactions.sql
+++ api/src/main/resources/db/migration/V5__Create_table_transactions.sql
@@ -4,7 +4,7 @@ CREATE TABLE transactions (
     counterparty_user_id INTEGER,
     type TEXT, 
     value NUMERIC(12,2) NOT NULL,
-    datetime TIMESTAMP NOT NULL,
+    datetime TIMESTAMP WITH TIME ZONE NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (counterparty_user_id) REFERENCES users(id) ON DELETE CASCADE
