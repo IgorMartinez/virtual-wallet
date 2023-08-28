1741014d065d49394246213eff410a644b0d6662
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-08-28 17:54:01 -0300

    Add OpenAPI documentation in transaction methods

--- api/src/main/java/br/com/igormartinez/virtualwallet/controllers/TransactionController.java
+++ api/src/main/java/br/com/igormartinez/virtualwallet/controllers/TransactionController.java
@@ -11,6 +11,9 @@ import br.com.igormartinez.virtualwallet.data.dto.TransactionDTO;
 import br.com.igormartinez.virtualwallet.data.dto.TransferTransactionDTO;
 import br.com.igormartinez.virtualwallet.enums.TransactionType;
 import br.com.igormartinez.virtualwallet.services.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 
 @RestController
@@ -20,16 +23,49 @@ public class TransactionController {
     @Autowired
     TransactionService service;
 
+    @Operation(
+        summary = "Make a deposit in a user account",
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
     @PostMapping("/deposit")
     public TransactionDTO deposit(@RequestBody @Valid PersonalTransactionDTO transaction) {
         return service.createPersonalTransaction(transaction, TransactionType.DEPOSIT);
     }
 
+    @Operation(
+        summary = "Make a withdrawal in a user account",
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
     @PostMapping("/withdrawal")
     public TransactionDTO withdrawal(@RequestBody @Valid PersonalTransactionDTO transaction) {
         return service.createPersonalTransaction(transaction, TransactionType.WITHDRAWAL);
     }
 
+    @Operation(
+        summary = "Make a tranfer between two user accounts",
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
     @PostMapping("/transfer")
     public TransactionDTO transfer(@RequestBody @Valid TransferTransactionDTO transaction) {
         return service.createTranfer(transaction);
